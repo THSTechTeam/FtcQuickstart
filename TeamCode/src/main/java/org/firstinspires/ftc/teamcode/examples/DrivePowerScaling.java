3daafe8469c11e0af3c21bab33ba7519d0fcd537
BrandonPacewic <pacewicbrandon@gmail.com>
2023-10-21 19:02:47 -0700

    Added drive power scaling example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/DrivePowerScaling.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.examples;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp(name = "Drive Power Scaling")
+public class DrivePowerScaling extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotorSimple leftFront = hardwareMap.get(DcMotorSimple.class, "leftFront");
+        DcMotorSimple leftBack = hardwareMap.get(DcMotorSimple.class, "leftBack");
+        DcMotorSimple rightFront = hardwareMap.get(DcMotorSimple.class, "rightFront");
+        DcMotorSimple rightBack = hardwareMap.get(DcMotorSimple.class, "rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double lx = gamepad1.left_stick_x;
+            double ly = gamepad1.left_stick_y;
+            double rx = gamepad1.right_stick_x;
+
+            double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+
+            double drivePower = 0.8 - (0.6 * gamepad1.right_trigger);
+
+            leftFront.setPower(((ly + lx + rx) / max) * drivePower);
+            leftBack.setPower(((ly - lx + rx) / max) * drivePower);
+            rightFront.setPower(((ly - lx - rx) / max) * drivePower);
+            rightBack.setPower(((ly + lx - rx) / max) * drivePower);
+        }
+    }
+}
