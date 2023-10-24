32a6fab3a67001187aeafa94ee316d5e7f862168
BrandonPacewic <pacewicbrandon@gmail.com>
2023-10-23 18:23:03 -0700

    Added new example files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/DrivePowerToggle.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.examples;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(name = "Drive Power Toggle")
+public class DrivePowerToggle extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotorSimple leftFront = hardwareMap.get(DcMotorSimple.class, "leftFront");
+        DcMotorSimple leftBack = hardwareMap.get(DcMotorSimple.class, "leftBack");
+        DcMotorSimple rightFront = hardwareMap.get(DcMotorSimple.class, "rightFront");
+        DcMotorSimple rightBack = hardwareMap.get(DcMotorSimple.class, "rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Deadline gamepadRateLimit = new Deadline(500, TimeUnit.MILLISECONDS);
+        double highPower = 0.8;
+        double lowPower = 0.6;
+        double currentPower = lowPower;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double lx = gamepad1.left_stick_x;
+            double ly = gamepad1.left_stick_y;
+            double rx = gamepad1.right_stick_x;
+
+            double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+
+            if (gamepadRateLimit.hasExpired() && gamepad1.a) {
+                if (currentPower == highPower) {
+                    currentPower = lowPower;
+                } else {
+                    currentPower = highPower;
+                }
+
+                gamepadRateLimit.reset();
+            }
+
+            leftFront.setPower(((ly + lx + rx) / max) * currentPower);
+            leftBack.setPower(((ly - lx + rx) / max) * currentPower);
+            rightFront.setPower(((ly - lx - rx) / max) * currentPower);
+            rightBack.setPower(((ly + lx - rx) / max) * currentPower);
+        }
+    }
+}
