d3421dc0db3d1577ab136f91057478c4cc8e8a6e
BrandonPacewic <pacewicbrandon@gmail.com>
2023-11-08 07:06:57 -0800

    New video files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/DriveByTimeAuto.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.examples.;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class DriveByTimeAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotorSimple frontLeft = hardwareMap.get(DcMotorSimple.class, "frontLeft");
+        DcMotorSimple backLeft = hardwareMap.get(DcMotorSimple.class, "backLeft");
+        DcMotorSimple frontRight = hardwareMap.get(DcMotorSimple.class, "frontRight");
+        DcMotorSimple backRight = hardwareMap.get(DcMotorSimple.class, "backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        ElapsedTime runtime = new ElapsedTime();
+
+        waitForStart();
+
+        frontLeft.setPower(0.5);
+        backLeft.setPower(0.5);
+        frontRight.setPower(0.5);
+        backRight.setPower(0.5);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            idle();
+        }
+
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        frontRight.setPower(0);
+        backRight.setPower(0);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 0.5)) {
+            idle();
+        }
+
+        frontLeft.setPower(-0.5);
+        backLeft.setPower(-0.5);
+        frontRight.setPower(0.5);
+        backRight.setPower(0.5);
+
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            idle();
+        }
+
+        requestOpModeStop();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/DrivePowerToggle2.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.examples;
+
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp(name = "Drive Power Toggle 2")
+public class DrivePowerToggle2 extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DcMotorSimple leftFront  = hardwareMap.get(DcMotorSimple.class, "leftFront");
+        DcMotorSimple leftBack   = hardwareMap.get(DcMotorSimple.class, "leftBack");
+        DcMotorSimple rightFront = hardwareMap.get(DcMotorSimple.class, "rightFront");
+        DcMotorSimple rightBack  = hardwareMap.get(DcMotorSimple.class, "rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        Gamepad previous1 = new Gamepad();
+        double lowPower = 0.6;
+        double highPower = 0.8;
+        double currentPower = lowPower;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double lx = gamepad1.left_stick_x;
+            double ly = gamepad1.left_stick_y;
+            double rx = gamepad1.right_stick_x;
+
+            double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+
+            if (gamepad1.a && !previous1.a) {
+                if (currentPower == lowPower) {
+                    currentPower = highPower;
+                } else {
+                    currentPower = lowPower;
+                }
+            }
+
+            leftFront.setPower(((ly + lx + rx) / max) * currentPower);
+            leftBack.setPower(((ly - lx + rx) / max) * currentPower);
+            rightFront.setPower(((ly - lx - rx) / max) * currentPower);
+            rightBack.setPower(((ly + lx - rx) / max) * currentPower);
+
+            previous1.copy(gamepad1);
+        }
+    }
+}
